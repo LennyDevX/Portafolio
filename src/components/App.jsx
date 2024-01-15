290b387f5f7945cb7ce0b27ce38245188d14d0a3
lennymercadovaldez <71667982+lennymercadovaldez@users.noreply.github.com>
2024-01-14 20:30:52 -0500

    Update 0.1

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import HeroSection from './HeroSection.jsx' // Asegúrate de que la ruta de importación sea correcta
+import Navbar from './Navbar.jsx' // Asegúrate de que la ruta de importación sea correcta
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+    </>
+  )
+}
+
+export default App
